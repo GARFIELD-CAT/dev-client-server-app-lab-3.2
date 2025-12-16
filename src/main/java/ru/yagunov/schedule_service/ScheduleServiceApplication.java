0d38a078a14ce8e0aa0da755471be0482f594fd5
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-12-16 20:20:09 +0300

    init schedule service

---
+++ src/main/java/ru/yagunov/schedule_service/ScheduleServiceApplication.java
@@ -0,0 +1,14 @@
+package ru.yagunov.schedule_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleServiceApplication.class, args);
+        System.out.println("Ok");
+	}
+
+}
